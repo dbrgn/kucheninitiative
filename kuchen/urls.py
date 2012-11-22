8898bf96364debbca49faabdd025ae92b1fb1c3e
Danilo Bargen <gezuru@gmail.com>
2012-11-22 23:33:31 +0100

    Added robots.txt

--- kuchen/urls.py
+++ kuchen/urls.py
@@ -3,12 +3,16 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^robots.txt$', direct_to_template,
+        {'template': 'front/robots.txt', 'mimetype': 'text/plain'}),
 
     url(r'', include('front.urls')),
 )
