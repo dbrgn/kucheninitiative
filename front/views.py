cea1a4760e6864ab6b2b9a240d86c27a72bc2ef3
Danilo Bargen <gezuru@gmail.com>
2012-11-22 23:19:54 +0100

    Random content on home page

--- front/views.py
+++ front/views.py
@@ -1,9 +1,15 @@
+from datetime import datetime
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth import models as auth_models
 from front import models
 
 class HomeView(TemplateView):
     template_name = 'front/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['switch'] = datetime.now().second % 2
+        return context
 
 class MemberView(ListView):
     template_name = 'front/members.html'
