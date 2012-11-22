56392097ff015eafb6451b0695cf7350c5de97ad
Danilo Bargen <gezuru@gmail.com>
2012-11-22 12:56:10 +0100

    Initial version

---
+++ kuchen/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'kuchen.views.home', name='home'),
+    # url(r'^kuchen/', include('kuchen.foo.urls')),
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
