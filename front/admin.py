7b3fbe983a1238ebadbfd235afced865783d82f5
Danilo Bargen <gezuru@gmail.com>
2012-11-26 08:47:51 +0100

    Tuned user admin page

--- front/admin.py
+++ front/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import models as auth_models
 from front import models
 
-admin.site.register(models.UserProfile)
+
+# User management
+
+class UserProfileInline(admin.StackedInline):
+    model = models.UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(auth_models.User)
+admin.site.register(auth_models.User, UserAdmin)
+
+
+# Other model admin registrations
+
 admin.site.register(models.Assignment)
 admin.site.register(models.Semester)
