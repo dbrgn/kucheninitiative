93d9c8b8478c6827833c3fb8ba2e04cd3e47a003
Danilo Bargen <gezuru@gmail.com>
2012-12-21 21:42:43 +0100

    Show member count on home page

--- front/views.py
+++ front/views.py
@@ -13,6 +13,7 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['switch'] = datetime.now().second % 2
+        context['membercount'] = auth_models.User.objects.count()
         return context
 
 
